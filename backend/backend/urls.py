ab11fdac9dce74059106e88628bce7884fa03e88
JANUFA KARONA <ajanufakarona@gmail.com>
2026-03-16 20:01:09 +0530

    update portfolio images

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 from django.urls import path, include
 from backend.contact.views import frontend
 
-def home(request):
-    return HttpResponse("Django backend is running successfully!")
+
 
 urlpatterns = [
     path("", frontend),
